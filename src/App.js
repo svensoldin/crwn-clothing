729b4ef78d49e1348b1df1e35c7c90323ed47d74
Sven Soldin <soldinsven@gmail.com>
2020-05-04 18:24:16 +0200

    created sign-in component and page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Switch, Route } from 'react-router-dom';
 import Homepage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component.jsx';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx';
 
 
 function App() {
@@ -12,7 +13,8 @@ function App() {
     	<Header />
     	<Switch>
     		<Route exact path='/' component={Homepage} />
-    		<Route patch='/shop' component={ShopPage} />
+    		<Route path='/shop' component={ShopPage} />
+    		<Route path='/signin' component={SignInAndSignUpPage} />
     	</Switch>
     </div>
   );
