63c783d3e66795ed880287946e7cd0360bc49a0f
Sven Soldin <soldinsven@gmail.com>
2020-05-04 16:57:01 +0200

    updating app with shop page, collection-previex component and collection-item comopnent

--- src/App.js
+++ src/App.js
@@ -2,21 +2,15 @@ import React from 'react';
 import './App.css';
 import { Switch, Route } from 'react-router-dom';
 import Homepage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
-const HatsPage = () => {
-	return (
-		<div>
-			<h1>HATS PAGE</h1>
-		</div>
-	)
-}
 
 function App() {
   return (
     <div>
     	<Switch>
     		<Route exact path='/' component={Homepage} />
-    		<Route patch='/hats' component={HatsPage} />
+    		<Route patch='/shop' component={ShopPage} />
     	</Switch>
     </div>
   );
