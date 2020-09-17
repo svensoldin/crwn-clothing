da45d4081b34c3fc8289d84c738d5bdc2486741f
Sven Soldin <soldinsven@gmail.com>
2020-09-17 17:23:55 +0200

    updated readme

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,9 +1,9 @@
-import React from "react";
-import MenuItem from "../menu-item/menu-item.component";
-import "./directory.styles.scss";
-import { connect } from "react-redux";
-import { createStructuredSelector } from "reselect";
-import { selectDirectorySections } from "../../redux/directory/directory.selectors";
+import React from 'react';
+import MenuItem from '../menu-item/menu-item.component';
+import './directory.styles.scss';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+import { selectDirectorySections } from '../../redux/directory/directory.selectors';
 
 const Directory = ({ sections }) => (
 	<div className="directory-menu">
